534d43e8cde4dbebaf31087d529b351dbb936657
AndrewFang2 <AndrewFang6@gmail.com>
2021-03-20 03:01:04 -0400

    first commit

---
+++ Models/AnswerImage.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace Lab5.Models
+{
+    public enum Question
+    {
+        Earth, Computer
+    }
+    public class AnswerImage
+    {
+        public int AnswerImageId { get; set; }
+
+        [Required]
+        [DisplayName("File Name")]
+        public string FileName { get; set; }
+
+        [Required]
+        [DisplayName("URL")]
+        [Url]
+        public string Url { get; set; }
+
+        [Required]
+        [DisplayName("Question")]
+        public Question Question { get; set; }
+
+    }
+}
